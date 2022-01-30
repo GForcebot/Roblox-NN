3a3f05cff58211b9577da3aa86f72787918468f3
GForcebot <geoffreys012@gmail.com>
2022-01-30 17:35:24 -0600

    Update NN Color.lua

--- NN Color.lua
+++ NN Color.lua
@@ -18,8 +18,162 @@ end
 function toyears(timeamount)
 	return tomonths(timeamount)/12.008219178
 end
+--Neural Network made by ChickenSauceSandwich, Discord: Bald man with no hair#8606
+--edited by me, GForcebot, Discord: G Kitteh Cat#7884
+local NeuralNetwork = {}
+function bool2int(bool)
+	if(bool == true) then
+		return 1
+	else
+		return 0
+	end
+end
+local learningRate = .15
+--sigmoid activation function
+function sigmoid(activation)
+	return 1.0/(1.0+math.exp(-activation))
+end
 
+function initNeuron(inputs)
+	local neuron = {weights = {}, inputs = {}, activation = 0, delta = 0}
 
+	--initialize input weights 
+	for w=1,inputs do
+		table.insert(neuron.weights, math.random()*(1-(-1)) - 1)
+	end
+
+	--initialize bias weight
+	table.insert(neuron.weights, 0)
+
+	return neuron
+end
+
+function getActivation(neuron, activation)
+	local activation = neuron.weights[#neuron.weights]
+
+	for w=1,#neuron.weights - 1 do
+		activation += neuron.inputs[w] * neuron.weights[w]
+	end
+
+	if(activation=="sigmoid") then
+		activation = sigmoid(activation)
+	elseif(activation=="tanh") then
+		activation = math.tanh(activation)
+	elseif(activation=="ReLU") then
+		activation = math.max(activation * 0.01, activation)
+	end
+
+	return activation
+end
+
+--get slope of activation
+function transferDerivative(activationf, activation)
+	if(activationf=="sigmoid") then
+		activation = sigmoid(activation) * (1 - sigmoid(activation))
+	elseif(activationf=="tanh") then
+		activation = 1 - math.tanh(activation)^2
+	elseif(activationf=="ReLU") then
+		activation = 1 * bool2int((activation > 0))
+	end
+
+	return activation
+end
+
+--calculates network output
+function propogateForward(input)
+	local oldInput = {}
+	for l,layer in pairs(NeuralNetwork) do
+		local newInput = {}
+		for n,neuron in pairs(layer) do
+			if(#neuron.inputs > 0) then neuron.inputs = {} end
+
+			if(l == 1) then
+				table.insert(neuron.inputs, input[n])
+			else
+				neuron.inputs = oldInput
+			end
+
+			--get activation
+			local activation = getActivation(neuron,"sigmoid")
+			neuron.activation = activation
+			table.insert(newInput, activation)
+		end
+		oldInput = newInput
+		newInput = {}
+	end
+	return oldInput
+end
+--calculates the error of the network
+function propogateBackwards(label)
+	local lastLayer = {}
+	for l=#NeuralNetwork,1,-1 do
+		local losses = {}
+		if(l ~= #NeuralNetwork) then
+			for n,neuron in pairs(NeuralNetwork[l]) do
+				--calculate loss of hidden neurons
+				local loss = 0
+				for pn,pneuron in pairs(lastLayer) do
+					loss += pneuron.weights[n] * pneuron.delta
+				end
+				table.insert(losses,loss)
+			end
+		else
+			for n,neuron in pairs(NeuralNetwork[l]) do
+				table.insert(losses,neuron.activation - label[n])
+			end
+		end
+
+		for n,neuron in pairs(NeuralNetwork[l]) do
+			neuron.delta = losses[n] * transferDerivative("sigmoid", neuron.activation)
+		end
+
+		losses = {}
+		lastLayer = NeuralNetwork[l]
+	end
+end
+
+--function to update neuron weights
+function updateNetwork()
+	for l,layer in pairs(NeuralNetwork) do
+		for n,neuron in pairs(layer) do
+			for i,input in pairs(neuron.inputs) do
+				neuron.weights[i] -= learningRate * neuron.delta * neuron.inputs[i]
+			end
+
+			--update bias weight
+			neuron.weights[#neuron.weights] -= learningRate * neuron.delta
+		end
+	end
+end
+--input layer
+local layers = {}
+for i = 1,3 do
+	local neur = initNeuron(1)
+	table.insert(layers,neur)
+end
+table.insert(NeuralNetwork, layers)
+--hidden
+local layers = {}
+for i = 1,7 do
+	local neuron = {}
+	neuron = initNeuron(3)
+	table.insert(layers,neuron)
+end
+table.insert(NeuralNetwork,layers)
+local layers = {}
+for i = 1,7 do
+	local neuron = {}
+	neuron = initNeuron(7)
+	table.insert(layers,neuron)
+end
+table.insert(NeuralNetwork,layers)
+--output
+local layers = {}
+for i = 1,3 do
+	local neuron = initNeuron(7) 
+	table.insert(layers,neuron)
+end
+table.insert(NeuralNetwork,layers)
 	local debris = game:GetService("Debris")
 	local max = 1016.8157
 	local worldmodel = Instance.new("WorldModel",script)
