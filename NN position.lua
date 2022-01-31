c7313b562fa49d6462be3e349e2079780610e7ff
GForcebot <geoffreys012@gmail.com>
2022-01-31 15:51:05 -0600

    Update NN position.lua

--- NN position.lua
+++ NN position.lua
@@ -1,8 +1,6 @@
 --Neural Network made by ChickenSauceSandwich, Discord: Bald man with no hair#8606
 --edited by me, GForcebot, Discord: G Kitteh Cat#7884
 local NeuralNetwork = {}
-	--time coversions
-	===
 	function bool2int(bool)
 		if(bool == true) then
 		return 1
