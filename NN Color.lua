ae4ac9f07b371c2b168932a4beccac26c460ac41
GForcebot <geoffreys012@gmail.com>
2022-01-31 15:48:41 -0600

    Update NN Color.lua

--- NN Color.lua
+++ NN Color.lua
@@ -1,24 +1,4 @@
-oldtime = os.time()
 
---time coversions
-function tomins(timeamount)
-	return (timeamount)/60
-end
-function tohours(timeamount)
-	return tomins(timeamount)/60
-end
-function todays(timeamount)
-	return tohours(timeamount)/24
-end
-function toweeks(timeamount)
-	return todays(timeamount)/7
-end
-function tomonths(timeamount)
-	return toweeks(timeamount)/4.3452380952
-end
-function toyears(timeamount)
-	return tomonths(timeamount)/12.008219178
-end
 
 --Neural Network made by ChickenSauceSandwich, Discord: Bald man with no hair#8606
 --edited by me, GForcebot, Discord: G Kitteh Cat#7884
@@ -217,61 +197,8 @@ task.wait(.01)
 		block.Color = Color3.new(output[1],output[2],output[3])
 		block.Size = Vector3.one
 	if block.Color ==  desired then
-			block.Transparency = 0
-		if math.round(os.time()-oldtime) <= 60 then
-			if  math.round(os.time()-oldtime) ~= 1 then
-				print("Try # "..i.. " did it. It took "..(tomins(os.time()-oldtime)).. " seconds.")
-				debris:AddItem(block,.1) break
-			elseif math.round(os.time()-oldtime) == 1 then
-				print("Try # "..i.. " did it. It took "..(tomins(os.time()-oldtime)).. "second.")
-				debris:AddItem(block,.1) break
-			elseif math.round(tomins(os.time()-oldtime)) ~= 1 then
-				print("Try # "..i.. " did it. It took "..math.round(tomins(os.time()-oldtime)).. " minutes.")
-				debris:AddItem(block,.1) break
-			elseif math.round(tomins(os.time()-oldtime)) == 1 then
-				print("Try # "..i.. " did it. It took "..math.round(tomins(os.time()-oldtime)).. " minute.")
-				debris:AddItem(block,.1) break
-			elseif math.round(tomins(os.time()-oldtime)) <= 60 then
-				if math.round(tohours(os.time()-oldtime)) ~= 1 then
-					print("Try # "..i.. " did it. It took "..math.round(tomins(os.time()-oldtime)).. " hours.")
-					debris:AddItem(block,.1) break
-				elseif math.round(tohours(os.time()-oldtime)) == 1 then
-					print("Try # "..i.. " did it. It took "..math.round(tomins(os.time()-oldtime)).. " hour.")
-					debris:AddItem(block,.1) break
-				end
-			elseif math.round(tohours(os.time()-oldtime)) <= 24 then
-				if math.round(todays(os.time()-oldtime)) ~= 1 then
-					print("Try # "..i.. " did it. It took "..math.round(tomins(os.time()-oldtime)).. "days.")
-					debris:AddItem(block,.1) break
-				elseif math.round(todays(os.time()-oldtime)) == 1 then
-					print("Try # "..i.. " did it. It took "..math.round(tomins(os.time()-oldtime)).. "day.")
-					debris:AddItem(block,.1) break
-				end
-			elseif math.round(todays(os.time()-oldtime)) <= 7 then
-				if math.round(toweeks(os.time()-oldtime)) ~= 1 then
-					print("Try # "..i.. " did it. It took "..math.round(tomins(os.time()-oldtime)).. "weeks.")
-					debris:AddItem(block,.1) break
-				elseif math.round(toweeks	(os.time()-oldtime)) == 1 then
-					print("Try # "..i.. " did it. It took "..math.round(tomins(os.time()-oldtime)).. "week.")
-					debris:AddItem(block,.1) break
-				end
-			elseif math.round(toweeks(os.time()-oldtime)) <= 4 then
-				if math.round(tomonths(os.time()-oldtime)) ~= 1 then
-					print("Try # "..i.. " did it. It took "..math.round(tomins(os.time()-oldtime)).. "months.")
-					debris:AddItem(block,.1) break
-				elseif math.round(tomonths(os.time()-oldtime)) == 1 then
-					print("Try # "..i.. " did it. It took "..math.round(tomins(os.time()-oldtime)).. "month.")
-					debris:AddItem(block,.1) break
-				end
-			elseif math.round(tomonths(os.time()-oldtime)) <= 12 then
-				if math.round(toyears(os.time()-oldtime)) ~= 1 then
-					print("Try # "..i.. " did it. It took "..math.round(tomins(os.time()-oldtime)).. "years.")
-					debris:AddItem(block,.1) break
-				elseif math.round(toyears(os.time()-oldtime)) == 1 then
-					print("Try # "..i.. " did it. It took "..math.round(tomins(os.time()-oldtime)).. "year.")
-					debris:AddItem(block,.1) break
-				end
-			end
-		end
-end
+		block.Transparency = 0
+		print("Try # "..i.. " did it.")
+		debris:AddItem(block,.1) break
 	end
+end
